1bfefc55edd5ad0cf7f75c046fba57e476be32e6
Mattia Panzeri <dev@mattiapanzeri.it>
2022-11-16 00:45:32 +0000

    test: increase test coverage to 100% 🎉

--- test/authClient.spec.ts
+++ test/authClient.spec.ts
@@ -1,7 +1,10 @@
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-import { createMockAuthClient } from './test-utils';
+import {
+  createMockAuthClient,
+  createMockAuthClientWithHooks,
+} from './test-utils';
 
 afterEach(require('@testing-library/react').cleanup);
 
@@ -33,6 +36,21 @@ describe('AuthClient', () => {
       });
 
       expect(initFailureEventListener).toHaveBeenCalledTimes(1);
+    });
+
+    it('should invoke postInit hook', async () => {
+      const postInitHook = jest.fn();
+
+      const authClientStub = createMockAuthClientWithHooks({
+        onPostInit: postInitHook,
+      });
+      jest.spyOn(authClientStub, 'onInit').mockResolvedValue(undefined);
+
+      await rtl.act(async () => {
+        await authClientStub.init();
+      });
+
+      expect(postInitHook).toHaveBeenCalledTimes(1);
     });
   });
 
@@ -79,6 +97,46 @@ describe('AuthClient', () => {
       });
 
       expect(loginFailureEventListener).toHaveBeenCalledTimes(1);
+    });
+
+    it('should invoke preLogin and postLogin hooks in case of success', async () => {
+      const preLoginHook = jest.fn();
+      const postLoginHook = jest.fn();
+
+      const authClientStub = createMockAuthClientWithHooks({
+        onPreLogin: preLoginHook,
+        onPostLogin: postLoginHook,
+      });
+      jest.spyOn(authClientStub, 'onLogin').mockResolvedValue({
+        authToken: 'tkn',
+        refreshToken: 'tkn',
+      });
+
+      await rtl.act(async () => {
+        await authClientStub.login();
+      });
+
+      expect(preLoginHook).toHaveBeenCalledTimes(1);
+      expect(postLoginHook).toHaveBeenCalledTimes(1);
+      expect(postLoginHook).toHaveBeenCalledWith(true);
+    });
+
+    it('should invoke preLogin and postLogin hooks in case of failure', async () => {
+      const preLoginHook = jest.fn();
+      const postLoginHook = jest.fn();
+
+      const authClientStub = createMockAuthClientWithHooks({
+        onPreLogin: preLoginHook,
+        onPostLogin: postLoginHook,
+      });
+
+      await rtl.act(async () => {
+        await authClientStub.login();
+      });
+
+      expect(preLoginHook).toHaveBeenCalledTimes(1);
+      expect(postLoginHook).toHaveBeenCalledTimes(1);
+      expect(postLoginHook).toHaveBeenCalledWith(false);
     });
   });
 
@@ -168,6 +226,46 @@ describe('AuthClient', () => {
       expect(refreshSuccessEventListener).toHaveBeenCalledTimes(1);
       expect(refreshFailureEventListener).toHaveBeenCalledTimes(0);
     });
+
+    it('should invoke preRefresh and postRefresh hooks in case of success', async () => {
+      const preRefreshHook = jest.fn();
+      const postRefreshHook = jest.fn();
+
+      const authClientStub = createMockAuthClientWithHooks({
+        onPreRefresh: preRefreshHook,
+        onPostRefresh: postRefreshHook,
+      });
+      jest.spyOn(authClientStub, 'onRefresh').mockResolvedValue({
+        authToken: 'tkn',
+        refreshToken: 'tkn',
+      });
+
+      await rtl.act(async () => {
+        await authClientStub.refresh();
+      });
+
+      expect(preRefreshHook).toHaveBeenCalledTimes(1);
+      expect(postRefreshHook).toHaveBeenCalledTimes(1);
+      expect(postRefreshHook).toHaveBeenCalledWith(true);
+    });
+
+    it('should invoke preRefresh and postRefresh hooks in case of failure', async () => {
+      const preRefreshHook = jest.fn();
+      const postRefreshHook = jest.fn();
+
+      const authClientStub = createMockAuthClientWithHooks({
+        onPreRefresh: preRefreshHook,
+        onPostRefresh: postRefreshHook,
+      });
+
+      await rtl.act(async () => {
+        await authClientStub.refresh();
+      });
+
+      expect(preRefreshHook).toHaveBeenCalledTimes(1);
+      expect(postRefreshHook).toHaveBeenCalledTimes(1);
+      expect(postRefreshHook).toHaveBeenCalledWith(false);
+    });
   });
 
   describe('on logout', () => {
@@ -210,6 +308,186 @@ describe('AuthClient', () => {
       });
 
       expect(logoutFailureEventListener).toHaveBeenCalledTimes(1);
+    });
+
+    it('should invoke preLogout and postLogout hooks in case of success', async () => {
+      const preLogoutHook = jest.fn();
+      const postLogoutHook = jest.fn();
+
+      const authClientStub = createMockAuthClientWithHooks({
+        onPreLogout: preLogoutHook,
+        onPostLogout: postLogoutHook,
+      });
+      jest.spyOn(authClientStub, 'onLogout').mockResolvedValue(undefined);
+
+      await rtl.act(async () => {
+        await authClientStub.logout();
+      });
+
+      expect(preLogoutHook).toHaveBeenCalledTimes(1);
+      expect(postLogoutHook).toHaveBeenCalledTimes(1);
+      expect(postLogoutHook).toHaveBeenCalledWith(true);
+    });
+
+    it('should invoke preLogout and postLogout hooks in case of failure', async () => {
+      const preLogoutHook = jest.fn();
+      const postLogoutHook = jest.fn();
+
+      const authClientStub = createMockAuthClientWithHooks({
+        onPreLogout: preLogoutHook,
+        onPostLogout: postLogoutHook,
+      });
+
+      await rtl.act(async () => {
+        await authClientStub.logout();
+      });
+
+      expect(preLogoutHook).toHaveBeenCalledTimes(1);
+      expect(postLogoutHook).toHaveBeenCalledTimes(1);
+      expect(postLogoutHook).toHaveBeenCalledWith(false);
+    });
+  });
+
+  describe('when requested', () => {
+    it('should return empty tokens by default', async () => {
+      const authClientStub = createMockAuthClient();
+
+      expect(authClientStub.tokens).toStrictEqual({});
+    });
+
+    it('should return current tokens after login', async () => {
+      const authClientStub = createMockAuthClient();
+      jest.spyOn(authClientStub, 'onLogin').mockResolvedValue({
+        authToken: 'a.fake.tkn',
+        refreshToken: 'a.fake.tkn',
+      });
+
+      await rtl.act(async () => {
+        await authClientStub.login();
+      });
+
+      expect(authClientStub.tokens).toStrictEqual({
+        authToken: 'a.fake.tkn',
+        refreshToken: 'a.fake.tkn',
+      });
+    });
+  });
+
+  describe('when event listener is removed', () => {
+    it('should not crash if no listener is defined', async () => {
+      const initSuccessEventListener = jest.fn();
+
+      const authClientStub = createMockAuthClient();
+
+      expect(() => {
+        authClientStub.off('initSuccess', initSuccessEventListener);
+      }).not.toThrow();
+
+      expect(initSuccessEventListener).not.toBeCalled();
+    });
+
+    it('should not be invoked on login success', async () => {
+      const loginSuccessEventListener = jest.fn();
+
+      const authClientStub = createMockAuthClient();
+      jest.spyOn(authClientStub, 'onLogin').mockResolvedValue({
+        authToken: 'tkn',
+        refreshToken: 'tkn',
+      });
+
+      authClientStub.on('loginSuccess', loginSuccessEventListener);
+
+      await rtl.act(async () => {
+        await authClientStub.login();
+      });
+
+      authClientStub.off('loginSuccess', loginSuccessEventListener);
+
+      await rtl.act(async () => {
+        await authClientStub.login();
+      });
+
+      expect(loginSuccessEventListener).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not be invoked on login failed', async () => {
+      const loginFailureEventListener = jest.fn();
+
+      const authClientStub = createMockAuthClient();
+      authClientStub.on('loginFailed', loginFailureEventListener);
+
+      await rtl.act(async () => {
+        await authClientStub.login();
+      });
+
+      authClientStub.off('loginFailed', loginFailureEventListener);
+
+      await rtl.act(async () => {
+        await authClientStub.login();
+      });
+
+      expect(loginFailureEventListener).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not be invoked on refresh success', async () => {
+      const refreshStartedListener = jest.fn();
+      const refreshSuccessEventListener = jest.fn();
+      const refreshFailureEventListener = jest.fn();
+
+      const authClientStub = createMockAuthClient();
+      jest.spyOn(authClientStub, 'onRefresh').mockResolvedValue({
+        authToken: 'tkn',
+        refreshToken: 'tkn',
+      });
+
+      authClientStub.on('refreshSuccess', refreshSuccessEventListener);
+      authClientStub.on('refreshStarted', refreshStartedListener);
+      authClientStub.on('refreshFailed', refreshFailureEventListener);
+
+      await rtl.act(() => {
+        authClientStub.refresh();
+      });
+
+      authClientStub.off('refreshSuccess', refreshSuccessEventListener);
+      authClientStub.off('refreshStarted', refreshStartedListener);
+      authClientStub.off('refreshFailed', refreshFailureEventListener);
+
+      await rtl.act(() => {
+        authClientStub.refresh();
+      });
+
+      expect(refreshStartedListener).toHaveBeenCalledTimes(1);
+      expect(refreshSuccessEventListener).toHaveBeenCalledTimes(1);
+      expect(refreshFailureEventListener).toHaveBeenCalledTimes(0);
+    });
+
+    it('should not be invoked on refresh failed', async () => {
+      const refreshStartedListener = jest.fn();
+      const refreshSuccessEventListener = jest.fn();
+      const refreshFailureEventListener = jest.fn();
+
+      const authClientStub = createMockAuthClient();
+      jest.spyOn(authClientStub, 'onRefresh').mockRejectedValue(null);
+
+      authClientStub.on('refreshSuccess', refreshSuccessEventListener);
+      authClientStub.on('refreshStarted', refreshStartedListener);
+      authClientStub.on('refreshFailed', refreshFailureEventListener);
+
+      await rtl.act(() => {
+        authClientStub.refresh();
+      });
+
+      authClientStub.off('refreshSuccess', refreshSuccessEventListener);
+      authClientStub.off('refreshStarted', refreshStartedListener);
+      authClientStub.off('refreshFailed', refreshFailureEventListener);
+
+      await rtl.act(() => {
+        authClientStub.refresh();
+      });
+
+      expect(refreshStartedListener).toHaveBeenCalledTimes(1);
+      expect(refreshSuccessEventListener).toHaveBeenCalledTimes(0);
+      expect(refreshFailureEventListener).toHaveBeenCalledTimes(1);
     });
   });
 });
--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -38,6 +38,68 @@ export const createMockAuthClient = () => {
   return new MockAuthClient();
 };
 
+export const createMockAuthClientWithHooks = (hooks: Record<string, any>) => {
+  // console.log('hooks', hooks);
+
+  class MockAuthClientWithHooks extends BaseAuthClient<
+    MockTokens,
+    MockCredentials
+  > {
+    onInit(): Promise<void> {
+      throw new Error('Method not implemented.');
+    }
+
+    protected onPostInit(): Promise<void> {
+      hooks['onPostInit']?.();
+      return Promise.resolve();
+    }
+
+    protected onPreLogin(): Promise<void> {
+      hooks['onPreLogin']?.();
+      return Promise.resolve();
+    }
+
+    onLogin(_credentials?: MockCredentials): Promise<MockTokens> {
+      throw new Error('Method not implemented.');
+    }
+
+    protected onPostLogin(isSuccess: boolean): Promise<void> {
+      hooks['onPostLogin']?.(isSuccess);
+      return Promise.resolve();
+    }
+
+    protected onPreRefresh(): Promise<void> {
+      hooks['onPreRefresh']?.();
+      return Promise.resolve();
+    }
+
+    onRefresh(_minValidity?: number): Promise<MockTokens> {
+      throw new Error('Method not implemented.');
+    }
+
+    protected onPostRefresh(isSuccess: boolean): Promise<void> {
+      hooks['onPostRefresh']?.(isSuccess);
+      return Promise.resolve();
+    }
+
+    protected onPreLogout(): Promise<void> {
+      hooks['onPreLogout']?.();
+      return Promise.resolve();
+    }
+
+    onLogout(): Promise<void> {
+      throw new Error('Method not implemented.');
+    }
+
+    protected onPostLogout(isSuccess: boolean): Promise<void> {
+      hooks['onPostLogout']?.(isSuccess);
+      return Promise.resolve();
+    }
+  }
+
+  return new MockAuthClientWithHooks();
+};
+
 export const createChild = (useAuthClientHook: () => MockAuthClient) => {
   return function() {
     const authClient = useAuthClientHook();
